1ee9abf8e1f92b1acdaa4d9ac3f941f8a7e86c5b
simonasalat <71327148+simonasalat@users.noreply.github.com>
2023-05-28 13:17:15 +0300

    Update wp-config.php

--- dictionary-system/wp-config.php
+++ dictionary-system/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'savokugrafoweb' );
+define( 'DB_NAME', 'dictionary-system' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
